aba2de69b763183bd30d57448e69141ba859b5a5
Git-Shira <shirae447@gmail.com>
2024-11-06 15:30:44 +0200

    Update index.js

--- Back/index.js
+++ Back/index.js
@@ -10,30 +10,7 @@ app.use(express.json());
 
 const cors = require("cors");
 
-const allowedOrigins = ['https://restaurant-website-gray-gamma.vercel.app'];
-
-app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-}));
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", allowedOrigins[0]);
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
-
-app.options('*', cors());
+app.use(cors());
 
 connectDB();
 app.use("/auth", authRoutes);
