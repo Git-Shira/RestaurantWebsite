4314b0cc4f3d62c8d27172224dfb4dd5b4aa7c9d
Git-Shira <shirae447@gmail.com>
2024-11-06 14:22:18 +0200

    Update index.js

--- Back/index.js
+++ Back/index.js
@@ -10,15 +10,34 @@ app.use(express.json());
 
 const cors = require("cors");
 
-app.use(cors({
-    origin: 'https://restaurant-website-gray-gamma.vercel.app', 
-    credentials: true 
-}));
+const allowedOrigins = ['https://restaurant-website-gray-gamma.vercel.app'];
 
-app.options('*', cors({
-    origin: 'https://restaurant-website-gray-gamma.vercel.app',
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true
 }));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://restaurant-website-gray-gamma.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
+app.options('*', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "https://restaurant-website-gray-gamma.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.sendStatus(200);
+});
 
 connectDB();
 app.use("/auth", authRoutes);
